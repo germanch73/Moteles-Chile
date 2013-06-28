5b8db0d2f361a79c9ef01c5f5feee95b0273f2ea
German Chavez <german@pc-209-68-160-190.cm.vtr.net>
2013-06-28 04:28:50 -0400

    Initial Commit

---
+++ Moteles Chile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Moteles Chile
+//
+//  Created by German Chavez on 28-06-13.
+//  Copyright (c) 2013 Naranja Software S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
